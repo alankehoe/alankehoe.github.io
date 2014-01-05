e2b6861e1039396505de8fb5de5cd03f2e0701c2
Alan Kehoe <alankehoe111@gmail.com>
2014-01-05 09:47:18 +0000

    clean

--- js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app', ['app.controllers', 'app.filters', 'app.directives']).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-            when('/', {templateUrl: 'partials/home.html', controller: 'MainCtrl'}).
-            when('/search', {templateUrl: 'partials/jquery-search.html', controller: 'SearchCtrl'}).
-            otherwise({redirectTo: '/'});
-    }]);
