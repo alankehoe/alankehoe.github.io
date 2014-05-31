4e701941c400a56b486a817817dfb241d7f05e1f
Alan Kehoe <alankehoe111@gmail.com>
2014-06-01 00:29:04 +0100

    Add loading bar to make sure pics are in place before load

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 angular.module('alankehoe.github.io', ['ui.bootstrap'])
 
-    .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.name = 'alan';
+    .controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+        $timeout(function () {
+            $scope.ready = true;
+        }, 1500);
     }]);
