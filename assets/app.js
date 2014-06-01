a4c595c9197f3971df5d046bf81850f080994962
Alan Kehoe <alankehoe111@gmail.com>
2014-06-01 09:59:12 +0100

    Add some interests

--- assets/app.js
+++ assets/app.js
@@ -3,5 +3,5 @@ angular.module('alankehoe.github.io', ['ui.bootstrap'])
     .controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
         $timeout(function () {
             $scope.ready = true;
-        }, 1500);
+        }, 1000);
     }]);
